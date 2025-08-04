b3ab663cd3302283362f5fd826be734dc5888d10
1718638143 <144675681+1718638143@users.noreply.github.com>
2025-08-04 12:54:17 +0800

    Update imgs.php

--- imgs.php
+++ imgs.php
@@ -1,28 +1,10 @@
 <?php
-//此php和保存链接的txt文件放在同一目录下
-$filename = "img.txt";  /*保存链接的txt文件名*/
-if(!file_exists($filename)){
-    die('文件不存在');
-}
-//从文本获取链接
-$pics = [];
-$fs = fopen($filename, "r");
-while(!feof($fs)){
-    $line=trim(fgets($fs));
-    if($line!=''){
-        array_push($pics, $line);
-    }
-}
-//从数组随机获取链接
-$pic = $pics[array_rand($pics)];
-//返回指定格式
-$type=$_GET['type'];
-switch($type){
-//JSON返回
-case 'json':
-    header('Content-type:text/json');
-    die(json_encode(['pic'=>$pic]));
-default:
-    die(header("Location: $pic"));
-}
+//初始化随机数生成器种子，这行代码也可以删除
+$seed = time();
+//获取随机数
+$num = rand(1,74);
+//拼接图片地址
+$picpath = "https://cdn.jsdelivr.net/gh/1718638143/img@master/img/".$num.".png";
+//重定位到图片
+die(header("Location: $picpath"));
 ?>
